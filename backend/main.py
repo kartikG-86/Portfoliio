95eab56ac089404fe0b7cc4788c35c9299ffc74d
Kartik Goyal <kartikgoyal0852@gmail.com>
2024-03-07 23:19:17 +0530

    add headers

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Flask,jsonify,request
 from flask_cors import CORS
 import smtplib 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE"],"allow_headers": ["Content-Type", "Authorization"]}})
 
 email = "kartikgoyal0852@gmail.com"
 password = "dgzfvqbpznsbcrry"
