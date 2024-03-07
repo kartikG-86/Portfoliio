cb68eb553c7cfc3b9e0a02a41b1c982b090bce71
Kartik Goyal <kartikgoyal0852@gmail.com>
2024-03-07 22:52:11 +0530

    changes in core

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Flask,jsonify,request
 from flask_cors import CORS
 import smtplib 
 app = Flask(__name__)
-CORS(app, origins='https://portfoliio-pzcr.vercel.app')
+CORS(app, origins='https://portfoliio-pzcr-9hdy2xgev-kartikg-86.vercel.app/')
 
 email = "kartikgoyal0852@gmail.com"
 password = "dgzfvqbpznsbcrry"
